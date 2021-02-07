9305d1442c2a46b422f0286ddf62042aa332ccec
original_Y <original_0413@163.com>
2021-02-07 13:32:20 +0800

    feat: sulli

---
+++ some_one_like_you.js
@@ -0,0 +1,12 @@
+/**
+ * Title : some_one_like_you <br>
+ * Company : yuyuyu <br>
+ * Description : - . -
+ *
+ *
+ * @author  stupid
+ * @version
+ * @date    2021/2/7 13:30
+ */
+import Sulli from './sulli'
+export default Sulli
